3aaf685da63167ec9d5cf4712b49a1e921d5a4a3
Umar Ahmed <umarsheikh303@gmail.com>
2020-08-16 02:56:53 +0500

    UPDATE

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Nav header="My First React Js Application"/>
+      <Nav header="My First React Js App"/>
       <Body caption="That's Why I love React !" />
       <Footer footer="Created By"/>
     </div>
